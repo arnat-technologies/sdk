e7f1e5f24a448a2a5659b08f661a67ca81bf2f39
Roy Surjano <roy.surjano@arnat.digital>
2020-06-07 12:30:00 -0500

    feat(eslint): ✨ adding base eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['simple-import-sort', '@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jasmine: true,
+    jest: true,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', 'jsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    }
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'simple-import-sort/sort': 'error',
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'prettier/prettier': ['error', { singleQuote: true }],
+    '@typescript-eslint/indent': 'off',
+  },
+};
