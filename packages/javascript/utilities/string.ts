34b7f986f0dc10c4bd09a94528e94993a2e6bcc4
Roy Surjano <roy.surjano@arnat.digital>
2020-06-14 08:57:26 -0500

    refactor(javascript-string): ♻️ adding javascript string updates

--- packages/javascript/utilities/string.ts
+++ packages/javascript/utilities/string.ts
@@ -390,3 +390,25 @@ export const uppercaseWords = (str) =>
 //
 // Or
 export const uppercaseWords = (str) => str.replace(/^(.)|\s+(.)/g, (c) => c.toUpperCase());
+
+// Check if a string contains only letters
+//
+//
+//  // Examples
+//  isAlpha('helloworld');          // true
+//  isAlpha('HelloWorld');          // true
+//  isAlpha('hello world');         // false
+//  isAlpha('0123456789');          // false
+export const isAlpha = (str) => /^[A-Z]+$/i.test(str);
+
+// Strip ANSI codes from a string
+//
+//
+// // Example
+// stripAnsiCodes('\u001B[4mcake\u001B[0m'); // 'cake'
+// stripAnsiCodes(
+//   '\u001B[0m\u001B[4m\u001B[42m\u001B[31mfoo\u001B[39m\u001B[49m\u001B[24mfoo\u001B[0m',
+// ); // 'foofoo'
+//
+export const stripAnsiCodes = (str) =>
+  str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
