3899aea1040dc11a33408b6100bfe5481360e69f
Roy Surjano <roy@surjano.xyz>
2020-08-21 17:13:52 -0500

    docs(documentation): 📝 updating docs & library

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '*.+(ts|tsx|jsx)': ['eslint', 'prettier-eslint --write'],
+  '*.+(css|scss)': ['stylelint', 'prettier-stylelint --write'],
+  // '*.+(ts|tsx|jsx)': ['prettier-eslint --write'],
+  // '*.+(css|scss)': ['prettier-stylelint --write'],
+};
